7bb7ab7134bd17e823200e2608ae674c96351d97
Rahul Sonti <36927413+rahulsonti@users.noreply.github.com>
2021-01-29 22:27:04 -0800

    Add files via upload

---
+++ C_R_T_Concatination.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 18:18:48 2020
+
+@author: vkaus
+"""
+import glob
+import sqlite3
+import csv
+
+
+conn = sqlite3.connect(r'C_R_T.db')
+cursor = conn.cursor()  
+
+def Turkey_DB(path):    
+    cursor.execute('create table if not exists turkey(Screen_Name text, Profile_Description text,Tweet_time date,Tweet_text text, Region text)')    
+    allfiles = []
+    allfiles = glob.glob(path)
+    for f in allfiles:
+        with open(f,mode='r',encoding="utf") as my_file:
+            list_ = []
+            for row in csv.DictReader(my_file):
+                 row_dict = {}
+                 new_list = []
+                 row_dict['Screen_Name']= row['user_screen_name']
+                 row_dict['Profile_Description']=row['user_profile_description']
+                 row_dict['Tweet_time']= row['tweet_time']                
+                 row_dict['Tweet_text']= row['tweet_text']
+                 row_dict['Region']= 'Turkey'
+                 new_list.append(row_dict['Screen_Name'])
+                 new_list.append(row_dict['Profile_Description'])
+                 new_list.append(row_dict['Tweet_time'])
+                 new_list.append(row_dict['Tweet_text'])
+                 new_list.append(row_dict['Region'])
+                 list_.append(new_list)
+            cursor.executemany("INSERT INTO turkey VALUES(?, ?, ?, ?, ?)", list_)
+            conn.commit()
+
+
+
+def Russia_DB(path_1):   
+    cursor.execute('create table if not exists russia(Screen_Name text, Profile_Description text,Tweet_time date,Tweet_text text, Region text)')
+    allfiles = []
+    allfiles = glob.glob(path_1)
+    for f in allfiles:
+        with open(f,mode='r',encoding="utf") as my_file:
+            my_reader=csv.DictReader((line.replace('\0','') for line in my_file), delimiter=",")
+            list_ = []
+            for row in my_reader:
+                 row_dict = {}
+                 new_list = []
+                 row_dict['Screen_Name']= row['user_screen_name']
+                 row_dict['Profile_Description']=row['user_profile_description']
+                 row_dict['Tweet_time']= row['tweet_time']                
+                 row_dict['Tweet_text']= row['tweet_text']
+                 row_dict['Region']= 'Russia'
+                 new_list.append(row_dict['Screen_Name'])
+                 new_list.append(row_dict['Profile_Description'])
+                 new_list.append(row_dict['Tweet_time'])
+                 new_list.append(row_dict['Tweet_text'])
+                 new_list.append(row_dict['Region'])
+                 list_.append(new_list)               
+            cursor.executemany("INSERT INTO russia VALUES(?, ?, ?, ?, ?)", list_)
+            conn.commit()          
+  
+
+def China_DB(path_2):
+    cursor.execute('create table if not exists china(Screen_Name text, Profile_Description text,Tweet_time date,Tweet_text text, Region text)')
+    allfiles = []
+    allfiles = glob.glob(path_2)
+    for f in allfiles:
+        with open(f,mode='r',encoding="utf") as my_file:
+            list_ = []
+            for row in csv.DictReader(my_file):
+                 row_dict = {}
+                 new_list = []
+                 row_dict['Screen_Name']= row['user_screen_name']
+                 row_dict['Profile_Description']=row['user_profile_description']
+                 row_dict['Tweet_time']= row['tweet_time']                
+                 row_dict['Tweet_text']= row['tweet_text']
+                 row_dict['Region']= 'China'
+                 new_list.append(row_dict['Screen_Name'])
+                 new_list.append(row_dict['Profile_Description'])
+                 new_list.append(row_dict['Tweet_time'])
+                 new_list.append(row_dict['Tweet_text'])
+                 new_list.append(row_dict['Region'])
+                 list_.append(new_list)                
+            cursor.executemany("INSERT INTO china VALUES(?, ?, ?, ?, ?)", list_)
+            conn.commit()
+
+
+path = "G:/DAEN-690/Turkey/*.csv"     
+path_1 = "G:/DAEN-690/Russia/*.csv"  
+path_2 = "G:/DAEN-690/China/*.csv"     
+  
+Turkey_DB(path)
+China_DB(path_2)
+Russia_DB(path_1)
+
+
+conn.close() 
+
---
+++ Others_Concatination.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 21:22:01 2020
+
+@author: vkaus
+"""
+import json
+import pandas as pd
+import os
+import bz2
+import time
+import sqlite3
+
+
+start = time.time()
+
+conn = sqlite3.connect(r'Otherstraining.db')
+cursor = conn.cursor()
+
+def tarfiles_to_table_as_otherstraining(root_path, csv_file_name):
+    cursor.execute('create table if not exists otherstraining(Screen_Name text,ID text, Profile_Description text,Tweet_text text, Region text)')
+    root_path = root_path.replace("\\", "/")
+    i = 0
+    for root,_,files in os.walk(root_path):
+        json_data = []
+        for name in files:
+            path = os.path.join(root,name)
+            print("Processing file with name: ", path)                      
+            with bz2.open(path, "rb") as f: 
+                for line in f.readlines():
+                    line_dict = {}
+                    new_list = []
+                    json_dat = json.loads(line.decode('utf-8'))
+                    if (pd.notnull(json_dat.get('user'))) & (json_dat.get('delete') == None):
+                        line_dict['Screen_Name']=json_dat['user']['screen_name']
+                        line_dict['ID']=json_dat['user']['id_str']
+                        line_dict['Profile_Description']=json_dat['user']['description']
+                        line_dict['Tweet_text']=json_dat['text']
+                        line_dict['Region']='Others'
+                        new_list.append(line_dict['Screen_Name'])
+                        new_list.append(line_dict['ID'])
+                        new_list.append(line_dict['Profile_Description'])
+                        new_list.append(line_dict['Tweet_text'])
+                        new_list.append(line_dict['Region'])
+                        json_data.append(new_list)
+        cursor.executemany("INSERT INTO otherstraining VALUES(?, ?, ?, ?, ?)", json_data)
+        conn.commit()
+        print("Data written to file", csv_file_name+str("_")+str(i)+".csv")
+#        df = pd.DataFrame(json_data)
+#        df.to_csv(csv_file_name+str("_")+str(i)+".csv")
+        i += 1
+
+tarfiles_to_table_as_otherstraining(r"G:\DAEN-690\Others-training", "json_data")
+end = time.time()
+print("Runtime of the program is", {end - start})
+conn.close() 
+
+
+
+
+
+
+start = time.time()
+
+conn = sqlite3.connect(r'Others-testing.db')
+cursor = conn.cursor()
+
+def tarfiles_to_table_as_otherstesting(root_path, csv_file_name):
+    cursor.execute('create table if not exists otherstesting(Screen_Name text,ID text, Profile_Description text,Tweet_text text, Region text)')
+    root_path = root_path.replace("\\", "/")
+    i = 0
+    for root,_,files in os.walk(root_path):
+        json_data = []
+        for name in files:
+            path = os.path.join(root,name)
+            print("Processing file with name: ", path)                      
+            with bz2.open(path, "rb") as f: 
+                for line in f.readlines():
+                    line_dict = {}
+                    new_list = []
+                    json_dat = json.loads(line.decode('utf-8'))
+                    if (pd.notnull(json_dat.get('user'))) & (json_dat.get('delete') == None):
+                        line_dict['Screen_Name']=json_dat['user']['screen_name']
+                        line_dict['ID']=json_dat['user']['id_str']
+                        line_dict['Profile_Description']=json_dat['user']['description']
+                        line_dict['Tweet_text']=json_dat['text']
+                        line_dict['Region']='Others'
+                        new_list.append(line_dict['Screen_Name'])
+                        new_list.append(line_dict['ID'])
+                        new_list.append(line_dict['Profile_Description'])
+                        new_list.append(line_dict['Tweet_text'])
+                        new_list.append(line_dict['Region'])
+                        json_data.append(new_list)
+        cursor.executemany("INSERT INTO otherstesting VALUES(?, ?, ?, ?, ?)", json_data)
+        conn.commit()
+        print("Data written to file", csv_file_name+str("_")+str(i)+".csv")
+#        df = pd.DataFrame(json_data)
+#        df.to_csv(csv_file_name+str("_")+str(i)+".csv")
+        i += 1
+
+tarfiles_to_table_as_otherstesting(r"G:\DAEN-690\Others-testing", "json_data")
+
+end = time.time()
+
+print("Runtime of the program is", {end - start})
+
+conn.close() 
+
+
+
+
+
+# =============================================================================
+# SELECT *
+# FROM russia 
+# WHERE tweet_time BETWEEN '2019-06-01T00:00:00.00' AND '2019-06-30T23:59:59.999'
+# 
+# CREATE TABLE junerussia (Screen_Name text, Profile_Description text, Tweet_text text, Region text) 
+# INSERT INTO junerussia SELECT Screen_Name,Profile_Description,Tweet_text,Region FROM russia WHERE tweet_time BETWEEN '2019-06-01T00:00:00.00' AND '2019-06-30T23:59:59.999';
+# 
+# ATTACH DATABASE 'G:\DAEN-690\C_R_T.db' AS crt_db
+# 
+# SELECT DISTINCT(otherstraining.Screen_Name) FROM junerussia INNER JOIN otherstraining on junerussia.Screen_Name=otherstraining.Screen_Name or junerussia.Screen_Name = otherstraining.ID;
+# 
+# 
+# CREATE TABLE juneothersrussia (Screen_Name text,Tweet_text text, Region text)
+# 
+# INSERT INTO juneothersrussia SELECT * FROM otherstraining WHERE otherstraining.Screen_Name NOT IN (SELECT DISTINCT(otherstraining.Screen_Name) FROM junerussia INNER JOIN otherstraining on junerussia.Screen_Name=otherstraining.Screen_Name or junerussia.Screen_Name = otherstraining.ID);
+# =============================================================================
+
+
+
+
+
